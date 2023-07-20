0e060b8e7038d6aa4bffed2b9b0914e235d64517
cristopher.moccia <cristopher.moccia@accenture.com>
2023-07-20 10:21:26 +0200

    added import

--- src/main/java/it/pagopa/interop/performancetest/PerformanceTestApplication.java
+++ src/main/java/it/pagopa/interop/performancetest/PerformanceTestApplication.java
@@ -3,6 +3,8 @@ package it.pagopa.interop.performancetest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @SpringBootApplication
