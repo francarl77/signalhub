360f0af72497c41a9430db4133d3dd9c9e95ed26
Francesco Carlucci <francesco.carlucci@accenture.com>
2023-09-03 18:26:35 +0200

    Added limit to pull signal requests

--- script/k6/start.cmd
+++ script/k6/start.cmd
@@ -15,4 +15,6 @@ k6 run push_test.js -u500 -d10s -e TEST=1-webflux-sqs -e URL=http://internal-sig
 k6 run pull_test.js -u500 -d10s -e URL=http://internal-signalhub-lb-368682252.eu-south-1.elb.amazonaws.com/relational/pull-signal
 
 
-kubectl -n dev  port-forward service/postgres 5432:5432
+kubectl -n dev port-forward service/postgres 5432:5432
+kubectl -n dev rollout restart deployment/signalhub-deployment
+kubectl -n dev exec -it postgres-7f8f6cb5f7-dz6np -- bash
